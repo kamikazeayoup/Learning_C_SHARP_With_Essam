0f7ee41a42c598c4e1014494d509ab9d006c8f9d
kamikazeayoup <mohamedosamaayoup202676@gmail.com>
2023-03-24 01:50:05 +0200

    new files for the program

--- Program.cs
+++ Program.cs
@@ -3,45 +3,12 @@ using System;
 using System.Collections.Generic;
     class Program
     {
-        static void Main(string[] args)
+    static void Main(string[] args)
     {
-        //input the number of emplyees
-        Console.WriteLine("How many employees did you have ? ");
-        var NumberOfEmployees = Convert.ToInt32(Console.ReadLine());
+        //this program was made to check the date that you enter, using oop principles. 
 
-        //define array of employees
-        Employee[] employees = new Employee[NumberOfEmployees];
-        //take the input
-        for (int index = 0; index < NumberOfEmployees; index++)
-        {
-            //define object of class employee
-            Employee employee = new Employee();
-
-            Console.Write("What is your first name ? ");
-            employee.FirstName = Console.ReadLine();
-
-            Console.Write("What is your last name ? ");
-            employee.LastName = Console.ReadLine();
-            
-            Console.Write("what is your wage ?");
-            employee.Wage = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("How many hours do you work monthly ? ");
-            employee.Hours = Convert.ToInt32(Console.ReadLine());
-
-            employees[index] = employee;
-            Console.WriteLine("\n---------------------------------------------------------\n\n\n");
-
-
-        }
-        for (int index = 0; index< NumberOfEmployees; index++)
-        {
-             Employee.printvalues(employees[index]);
-
-        }
-
-
-
-
+        //leap year
+        Date d1 = new Date(29 , 2 , 2024);
+       Console.WriteLine(d1.PrintDate());
     }
 }
