8291a2c2fc480fb0d73cd32c94d80d1a4f71e43a
kamikazeayoup <mohamedosamaayoup202676@gmail.com>
2023-03-24 04:03:44 +0200

    proberity

---
+++ Dollar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    public class Dollar
+    {
+        //backing field 
+        private double _price;
+        // the structure of the proberity (set and get) 
+        public double Price
+        {
+         set
+        {
+          _price= SetValue(value);
+        }
+        get
+        {
+            return _price;
+        }
+         }
+
+       //automatic proberety
+       // public double Price { set; get; }
+       
+
+        private double SetValue(double value) =>  value > 0 ? _price : 0;
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,10 +5,10 @@ using System.Collections.Generic;
     {
     static void Main(string[] args)
     {
-        //this program was made to check the date that you enter, using oop principles. 
-
-        //leap year
-        Date d1 = new Date(29 , 2 , 2024);
-       Console.WriteLine(d1.PrintDate());
+        Dollar dlr = new Dollar
+        {
+            Price = -1
+        };
+        Console.WriteLine($"Your Value = {dlr.Price} your total = {dlr.Price * 100}");
     }
 }
